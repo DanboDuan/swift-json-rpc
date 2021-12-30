d255975ef0e096ff8ef3d4793e2beaa6dfbb3063
bob <bob170731@gmail.com>
2021-12-30 11:12:14 +0800

    optimize GetFreePort

--- Sources/Example/Util.swift
+++ Sources/Example/Util.swift
@@ -17,50 +17,47 @@ import Logging
 
 /// for test
 public enum Util {
-    static func isPortFree(_ port: UInt16) -> Bool {
+    public static func getFreePort() -> Int {
         let socketFileDescriptor = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
         if socketFileDescriptor == -1 {
-            return false
+            return 0
         }
 
         var address = sockaddr_in()
         let sizeOfSockkAddr = MemoryLayout<sockaddr_in>.size
+        var len = socklen_t(sizeOfSockkAddr)
         address.sin_len = __uint8_t(sizeOfSockkAddr)
         address.sin_family = sa_family_t(AF_INET)
-        address.sin_port = Int(OSHostByteOrder()) == OSLittleEndian ? _OSSwapInt16(port) : port
+        address.sin_port = 0 // Int(OSHostByteOrder()) == OSLittleEndian ? _OSSwapInt16(port) : port
         address.sin_addr = in_addr(s_addr: inet_addr("127.0.0.1"))
         address.sin_zero = (0, 0, 0, 0, 0, 0, 0, 0)
         var bind_addr = sockaddr()
         memcpy(&bind_addr, &address, Int(sizeOfSockkAddr))
 
-        if Darwin.bind(socketFileDescriptor, &bind_addr, socklen_t(sizeOfSockkAddr)) == -1 {
+        if Darwin.bind(socketFileDescriptor, &bind_addr, len) == -1 {
             release(socket: socketFileDescriptor)
-            return false
+            return 0
         }
         if listen(socketFileDescriptor, SOMAXCONN) == -1 {
             release(socket: socketFileDescriptor)
-            return false
+            return 0
+        }
+        let result = withUnsafeMutablePointer(to: &address) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                Darwin.getsockname(socketFileDescriptor, $0, &len)
+            }
+        }
+        var port: uint16 = 0
+        if result == 0 {
+            port = address.sin_port
         }
         release(socket: socketFileDescriptor)
-        return true
+        return Int(port)
     }
 
     private static func release(socket: Int32) {
         Darwin.shutdown(socket, SHUT_RDWR)
         close(socket)
-    }
-
-    static func getFreePort() -> UInt16 {
-        var portNum: UInt16 = 0
-        for i in 50000 ..< UInt16.max {
-            let isFree = isPortFree(UInt16(i))
-            if isFree {
-                portNum = in_port_t(i)
-                return portNum
-            }
-        }
-
-        return 0
     }
 
     static func setupLog() {
